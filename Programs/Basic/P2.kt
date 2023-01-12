2ae974fcff82defa2400cba2a117586c1a322591
Prastab Mukhopadhyay <92989287+Prastabm@users.noreply.github.com>
2023-01-12 21:04:47 +0530

    Create P2.kt

---
+++ Programs/Basic/P2.kt
@@ -0,0 +1,16 @@
+//Understanding variables
+//Class doesn't need to be defined in kotlin
+    fun main(args:Array<String>)
+    {
+        var x=12
+        var y=13.0
+        var z='a'
+        var a="Prastab"
+        
+        //print("X contains integer: $x") can also be used for printing
+        
+        println("X contains integer: "+ x)
+        println("Y contain float: "+ y)
+        println("Z contains character: "+z)
+        println("A contains string: "+ a)
+    }
